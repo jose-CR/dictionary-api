931bd384af0512e25a4fbd91db7963cbfdb4a8b1
Alejandro <hjosealejandro21@gmail.com>
2024-03-28 09:40:13 -0600

    database, factories and seeder finished

---
+++ Glossary-API/database/factories/CategoryFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $body = $this->faker->randomElement(['English word']);
+        return [
+            'category' => $body,
+        ];
+    }
+}
