b5400dd4d8e005a3d5f870ddb3ec7af11cbf1dcb
crowend <hjosealejandro21@gmail.com>
2024-10-01 10:54:47 -0600

    redirect view register to page 404

--- Glossary-API/app/Http/Kernel.php
+++ Glossary-API/app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'api.cors' => \App\Http\Middleware\ApiCors::class,
+        'check.routes' => \App\Http\Middleware\CheckRoutes::class,
     ];
 }
